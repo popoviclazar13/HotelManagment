51d052204687f87e80678b0e10215513dfbdbca5
popoviclazar13 <79926338+popoviclazar13@users.noreply.github.com>
2025-04-13 00:29:45 +0200

    Dodata Ikonica za aplikaciju

--- HotelManagment/App.xaml.cs
+++ HotelManagment/App.xaml.cs
@@ -86,7 +86,6 @@ namespace HotelManagment
             services.AddScoped<ApartmaniPage>();
             services.AddScoped<HomePage>();
             services.AddScoped<LoginPage>();
-            services.AddScoped<MainWindow>();
         }
     }
 
